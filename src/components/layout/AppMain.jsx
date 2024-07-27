f977439421c9f7b0538c9633909f2c9df51001dd
Matt Vasilev <vasilevmatt@niuitmo.ru>
2024-07-27 18:51:19 +0300

    Offline behaviour added

--- src/components/layout/AppMain.jsx
+++ src/components/layout/AppMain.jsx
@@ -1,3 +1,4 @@
+import { Online, Offline } from 'react-detect-offline'
 import SearchFilters from '../SearchFilters/SearchFilters'
 import TicketsList from '../TicketsList/TicketsList'
 
@@ -5,7 +6,12 @@ export default function AppMain() {
   return (
     <main className="search">
       <SearchFilters />
-      <TicketsList />
+      <Online>
+        <TicketsList />
+      </Online>
+      <Offline>
+        <p>Нет подключения к сети, попробуйте позже...</p>
+      </Offline>
     </main>
   )
 }
